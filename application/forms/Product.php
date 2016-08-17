b8c8d2e65c7a6e97ecd9907b73d7255bdb44da40
vladimir <vladimirdemetrebalan@gmail.com>
2016-08-17 16:55:21 +0300

    deleted some

--- application/forms/Product.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Application_Models_Product extends Zend_Db_Table_Row {
-    
-}
-?>
--- application/forms/Products.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Application_Models_Products extends Zend_Db_Table_Abstract{
-	protected $_name	='products';
-	protected $_primary	='id';
-	protected $_rowClass='Application_Models_Product';
-}
-
-?>
