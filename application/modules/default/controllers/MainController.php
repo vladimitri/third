85ff8dbc13013b69f0582b36c07814838bef4e62
vladimir <vladimirdemetrebalan@gmail.com>
2016-08-18 15:41:24 +0300

    cart

--- application/modules/default/controllers/MainController.php
+++ application/modules/default/controllers/MainController.php
@@ -4,7 +4,7 @@ class MainController extends Zend_Controller_Action{
     public function init(){
         $this->_helper->viewRenderer->setNoRender(true);
     }
-    public function listAction(){
+    public function getproductsAction(){
         try{
             $products = new Application_Models_Products();
             echo Zend_Json::encode(array(
@@ -17,8 +17,85 @@ class MainController extends Zend_Controller_Action{
                 'message' => $ex->getMessage()
             ));
         }
+    }
+
+    public function loginAction(){
+        $this->view->form = $form = new Application_Forms_Login();
+        if($this->getRequest()->getParam('id')!==null){
+            if($this->getRequest()->getParam('id')==USER && $this->getRequest()->getParam('password')==PASSWORD){
+                $admin=new Zend_Session_Namespace('admin');
+                $admin->name=$this->getRequest()->getParam('id');
+                $this->_helper->redirector->gotoUrl('/');
+            } else {
+                $this->view->message='Wrong login';
+            }
+        }
+    }
+
+    public function logoutAction(){
+        $admin = new Zend_Session_Namespace('admin');
+        unset($admin->name);
+        $this->_helper->redirector->gotoUrl('/');
+    }
+
+    public function cartAction(){
+
+        try{
+            $cart = new Zend_Session_Namespace('cart');
+            $products = new Application_Models_Products;
+            $data = array();
+            if(isset($cart->products)) {
+                $data = $products->fetchAll(array('id IN (?)' => array_keys($cart->products)))->toArray();
+            }
+            echo Zend_Json::encode(array(
+                'success' => true ,
+                'data' => $data,
+                'quantity' =>$cart->products
+            ));
+
+        } catch (Exception $ex) {
+            echo Zend_Json::encode(array(
+                'success' => false,
+                'message' => $ex->getMessage()
+            ));
+        }
 
     }
+
+    public function addtocartAction(){
+        if($this->getRequest()->getParam('id')){
+            $id = $this->getRequest()->getParam('id');
+            if($id) {
+                try {
+                    $cart = new Zend_Session_Namespace('cart');
+                    if (!isset($cart->products)) {
+                        $cart->products = array();
+                    }
+                    if (isset($cart->products[$id])) {
+                        $cart->products[$id]++;
+                    } else {
+                        $cart->products[$id] = 1;
+                    }
+                    echo Zend_Json::encode(array(
+                        'success' => true
+                    ));
+                } catch (Exception $ex) {
+                    echo Zend_Json::encode(array(
+                        'success' => false,
+                        'message' => $ex->getMessage()
+                    ));
+                }
+            } else {
+                echo Zend_Json::encode(array(
+                    'success' => false,
+                    'message' => 'no id'.'error 2'
+                ));
+            }
+
+
+        }
+    }
+
 
 }
 
